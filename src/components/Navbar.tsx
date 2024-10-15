fd08dced4f75edb334fe46e5b91beaa56eaba36a
ecc97 <carmonaedwin1123@gmail.com>
2024-10-15 09:38:29 -0500

    feat: Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,20 @@
+"use client"
+import React from 'react'
+import Link from 'next/link'
+
+function Navbar() {
+  return (
+    <nav className='flex justify-center w-full bg-cyan-400 py-5'>
+    <ul className='flex gap-5'>
+      <li>
+        <Link href="/listTasks" className='hover:underline'>Lista de Tareas</Link>
+      </li>
+      <li>
+        <Link href="/addTask" className='hover:underline'>Crear Tarea</Link>
+      </li>
+    </ul>
+  </nav>
+  )
+}
+
+export default Navbar
